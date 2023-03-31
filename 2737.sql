0e4f76c8852e719fa78f91fe3d070f0407e0a665
Yanagawa <107330192+Gabriel-Hernandess@users.noreply.github.com>
2023-03-31 19:39:53 -0300

    Create 2737.sql

---
+++ 2737.sql
@@ -0,0 +1,11 @@
+select name, customers_number from lawyers
+    where customers_number = (
+        select max(customers_number) from lawyers
+    ) union all
+
+select name, customers_number from lawyers
+    where customers_number = (
+        select min(customers_number) from lawyers 
+    ) union all
+
+select 'Average', round(AVG(customers_number), 0) from lawyers
